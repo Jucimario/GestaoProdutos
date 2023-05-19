a2e3e1435b7a82427e98ea416f42f6159962b0be
jucimario <jucimarioo@live.com>
2023-05-19 20:59:23 -0300

    EstruturandoControllers

---
+++ Api.GestaoProdutos/Controllers/v1/ProdutoController.cs
@@ -0,0 +1,39 @@
+﻿using Domain.GestaoProdutos.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Api.GestaoProdutos.Controllers.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        [HttpPost("AdicionarProduto")]
+
+        public ActionResult AdicionarProduto([FromBody] Produto produto)
+        {
+            return NoContent();
+        }
+
+        [HttpGet("ListarProdutos")]
+
+        public ActionResult<ICollection<Produto>> ListarProdutos()
+        {
+            return NoContent();
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Produto produto)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+      
+    }
+}
